73665bb3ac79d4cd4755c1694f912396d9e9cf49
qq8861068 <8861068@qq.com>
2024-05-27 03:00:35 +0800

    消消乐

---
+++ src/settings.540fd.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scene/start.fire",orientation:"",jsList:[],bundleVers:{internal:"d0832",resources:"fd9ed",main:"ee0be"}};
